1f4f130643253450ff476bf43eba852befaf2995
Gareth Evans <agrath@gmail.com>
2018-08-26 15:28:26 +1200

    Add web sample project

---
+++ src/PrimitiveSharp.Web/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace PrimitiveSharp.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = "" }
+            );
+        }
+
+        protected void Application_Start()
+        {
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
